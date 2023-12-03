65f35397740f8f45b9029d676675e1c9094cc2c7
yutong195 <renyutong360@gmail.com>
2023-12-03 01:47:20 -0500

    Updated the song player UI

--- js/song_player.js
+++ js/song_player.js
@@ -1,5 +1,3 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('togglePlayer').addEventListener('click', function() {
         var player = document.getElementById('musicPlayer');
@@ -14,8 +12,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
     var audio = new Audio();
     var basePath = './songs/';
-    var songs = ['Perfect_Ed Shareen.mp3','Photograph_Ed Shareen.mp3','River Flows In You_Yiruma.mp3',
-    'Drifting Cloud_Masaaki Kishibe.mp3','Miracle Mountain_Masaaki Kishibe.mp3']; // Replace these with your actual song names
+    var songs = [
+        'Perfect_Ed Shareen.mp3',
+        'Photograph_Ed Shareen.mp3',
+        'River Flows In You_Yiruma.mp3',
+        'Drifting Cloud_Masaaki Kishibe.mp3',
+        'Miracle Mountain_Masaaki Kishibe.mp3'
+    ]; // Replace these with your actual song names
     songs = songs.map(song => basePath + song); // Prepend base path to each song name
 
     var currentSongIndex = 0;
@@ -24,53 +27,54 @@ document.addEventListener('DOMContentLoaded', function() {
         audio.src = songs[index];
         audio.play();
         updateSongList();
-        updateSongInfoDisplay(); // New function call
+        updateSongInfoDisplay(); // Update song info
     }
-    
+
     function updateSongInfoDisplay() {
-        var songName = songs[currentSongIndex].split('/').pop(); // Get the file name
-        songName = songName.replace('.mp3', ''); // Remove .mp3 extension
-        var songParts = songName.split('_'); // Assuming format is Title-Author.mp3
-        var displayText = songParts[0] + ' by ' + songParts[1]; // Format: Title by Author
+        var songName = songs[currentSongIndex].split('/').pop().replace('.mp3', '');
+        var songParts = songName.split('_');
+        var displayText = songParts[0].replace('_', ' ') + ' by ' + songParts[1].replace('_', ' ');
         document.getElementById('currentSongInfo').textContent = displayText;
     }
-    
 
     function updateSongList() {
         var songList = document.getElementById('songList');
         songList.value = songs[currentSongIndex];
-        // Remove 'playing' class from all options
         for (var option of songList.options) {
             option.classList.remove('playing');
         }
-        // Add 'playing' class to current song
         songList.options[currentSongIndex].classList.add('playing');
     }
 
     document.getElementById('playPause').addEventListener('click', function() {
         if (audio.paused) {
+            this.innerHTML = "Pause";
             audio.play();
         } else {
+            this.innerHTML = "Play";
             audio.pause();
         }
     });
 
-    document.getElementById('nextSong').addEventListener('click', function() {
+    function nextSong() {
         currentSongIndex = (currentSongIndex + 1) % songs.length;
         playSong(currentSongIndex);
-    });
+    }
 
-    document.getElementById('prevSong').addEventListener('click', function() {
+    function prevSong() {
         currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
         playSong(currentSongIndex);
-    });
+    }
+
+    document.getElementById('nextSong').addEventListener('click', nextSong);
+
+    document.getElementById('prevSong').addEventListener('click', prevSong);
 
     audio.addEventListener('timeupdate', function() {
         var percentage = (audio.currentTime / audio.duration) * 100;
         document.getElementById('progressBar').style.width = percentage + '%';
     });
-    
-    // Add click event listener to the progress bar container
+
     document.getElementById('progressBarContainer').addEventListener('click', function(e) {
         var progressBarWidth = this.clientWidth;
         var clickPositionX = e.offsetX;
@@ -79,9 +83,11 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     document.getElementById('songList').addEventListener('change', function(e) {
-        currentSongIndex = songs.indexOf(e.target.value);
+        currentSongIndex = songs.indexOf(basePath + e.target.value);
         playSong(currentSongIndex);
     });
+
+    audio.addEventListener('ended', nextSong); // Auto-play next song when current song ends
 
     // Initial song load
     playSong(0);
