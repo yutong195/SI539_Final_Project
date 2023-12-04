5bb54c5a170aea2a1dc9ba79d5386187e1e90bab
yutong195 <renyutong360@gmail.com>
2023-12-04 17:38:29 -0500

    Add "skip to main" for accessibiliy

--- js/animation.js
+++ js/animation.js
@@ -1,11 +1,47 @@
-window.addEventListener("load", function() {
-    console.log("HIHI!")
+document.addEventListener('DOMContentLoaded', function() {
+    // ... (your existing code) ...
     document.querySelectorAll('.pop_out').forEach((el, i) => {
         el.style.animationDelay = `${i * 0.5}s`;
     })
 
+    var hciSection = document.querySelector('.hci_section_all');
+    var elementPosition = hciSection.offsetTop;
+    var isElementVisible = false;
 
-})
+    function checkScrollAndAnimate() {
+        var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+        
+        if (scrollPosition + window.innerHeight > elementPosition && !isElementVisible) {
+            hciSection.classList.remove('hci-section-hidden');
+            hciSection.classList.add('hci-section-visible');
+            isElementVisible = true;
+        } else if (scrollPosition + window.innerHeight < elementPosition && isElementVisible) {
+            hciSection.classList.remove('hci-section-visible');
+            hciSection.classList.add('hci-section-hidden');
+            isElementVisible = false;
+        }
+    }
+
+    window.addEventListener('scroll', checkScrollAndAnimate);
+
+});
+
+
+
+
+
+
+
+// window.addEventListener("load", function() {
+//     console.log("HIHI!")
+//     document.querySelectorAll('.pop_out').forEach((el, i) => {
+//         el.style.animationDelay = `${i * 0.5}s`;
+//     })
+
+
+// })
+
+
 
 
 
